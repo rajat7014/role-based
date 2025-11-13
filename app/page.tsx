34c7a06bcd08133fbe6ac094a5c4d126ecec19cf
rajat7014 <132184024+rajat7014@users.noreply.github.com>
2025-11-13 18:34:53 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
-// import Image from 'next/image'
-// import DashboardPage from './dashboard/page'
+import Image from 'next/image'
+import DashboardPage from './dashboard/page'
 // import { render, screen } from '@testing-library/react'
 
-// export default function Home() {
-//   return <><DashboardPage />
-//   expect(screen.getByText('Loading...')).toBeInTheDocument()
-//   </>
-// }
-
-import { render, screen } from '@testing-library/react'
-import DashboardPage from './dashboard/page'
-import test from 'node:test'
-import expect from 'expect'
-
-test('Shows loading initially', () => {
-  render(<DashboardPage />)
-  expect(screen.getByText('Loading...')).toBeInTheDocument()
-})
+export default function Home() {
+  return <DashboardPage />
+}
